1f80f0a515c7e27463530d8333ef53a73172abc7
lin <tcslforone@163.com>
2018-09-04 11:45:29 +0800

    lin

--- object_consumer/src/main/java/com/lin/boot/object_consumer/Service/HelloService.java
+++ object_consumer/src/main/java/com/lin/boot/object_consumer/Service/HelloService.java
@@ -2,12 +2,13 @@ package com.lin.boot.object_consumer.Service;
 
 import com.lin.boot.interface_common.orm.HelloBean;
 import com.lin.boot.interface_common.service.HelloServiceInterface;
+import fallback.HelloServiceFallBack;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 @Service
-@FeignClient("object-privider")
+@FeignClient(name = "object-privider",fallback = HelloServiceFallBack.class) //降级处理(请求量大响应量小)
 public interface HelloService extends HelloServiceInterface{
 }
